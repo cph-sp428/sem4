07412eb6a16dda054e0784fefd7ab0c9e942751d
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2024-02-03 17:40:51 +0100

    axios working

--- week1/ex3-typedeclaration/typeDeclaration.ts
+++ week1/ex3-typedeclaration/typeDeclaration.ts
@@ -1,1 +1,14 @@
 import axios from 'axios';
+
+async function fetchUsers() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return response.data;
+}
+
+async function fetchUserById(id: number) {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+    return response.data;
+}
+
+console.log(fetchUsers());
+console.log(fetchUserById(1));
