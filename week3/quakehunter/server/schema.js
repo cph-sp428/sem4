8221238b0f5575a0c50c5b07207fd235d0917ccb
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2024-02-12 21:03:42 +0100

    some more

---
+++ week3/quakehunter/server/schema.js
@@ -0,0 +1,36 @@
+const {gql} = require('apollo-server');
+
+//schema definition language
+const typeDefs = gql`
+    type Query {
+        quakes: [Quake]!
+        quake(id: ID!): Quake
+        me: User
+    }
+
+    type Quake {
+        id: ID!
+        location: String
+        magnitude: Float
+        when: String
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        password; String!
+    }
+
+    type Mutation {
+        # if false, save failed -- check errors
+        saveRecord(recordId: ID!): RecordUpdateResponse!
+        # if false, delete failed -- check errors
+        deleteRecord(recordId: ID!): RecordUpdateResponse!
+
+        signup(username: String!, email: String!, password: String!): String!
+        login(email: String!, password: String!): String!
+    }
+`;
+
+module.exports = typeDefs;
