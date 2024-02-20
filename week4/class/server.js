51e75c57478d81a93c99594d263440849b3225b0
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2024-02-20 11:53:35 +0100

    ex1 start

--- week4/class/server.js
+++ week4/class/server.js
@@ -15,10 +15,26 @@ async function run(){
         
         await client.connect();
 
-        const database = client.db("sample_weatherdata").collection("data");
+        const data = client.db("sample_weatherdata").collection("data");
         console.log("Connected to the database");
  
-        await database.findOne({_id: new ObjectId("5553a998e4b02cf7151190b8")}).then((result) => {
+        await data.findOne({_id: new ObjectId("5553a998e4b02cf7151190b8")}).then((result) => {
+            console.log(result);
+        });
+        console.log("Connected to the database");
+
+        const doc = {
+            name: "Sebastian",
+            age: 26,
+            city: "Copenhagen"
+        }
+
+        await data.insertOne(doc).then((result) => {
+            console.log(result);
+            console.log("Document inserted");
+        });
+
+        await db.find().limit(2).toArray().then((result) => {
             console.log(result);
         });
 
@@ -26,8 +42,6 @@ async function run(){
 
     } catch{
         console.log("Error");
-    } finally {
-        await client.close();
     }
 }
 
--- week4/class/src/server.ts
+++ week4/class/src/server.ts
@@ -15,13 +15,35 @@ async function run(){
         
         await client.connect();
 
-        const database = client.db("sample_weatherdata").collection("data");
+        const data = client.db("sample_weatherdata").collection("data");
         console.log("Connected to the database");
  
-        await database.findOne({_id: new ObjectId("5553a998e4b02cf7151190b8")}).then((result) => {
+        await data.findOne({_id: new ObjectId("5553a998e4b02cf7151190b8")}).then((result) => {
+            console.log(result);
+        });
+        console.log("Connected to the database");
+
+        const doc = {
+            name: "Sebastian",
+            age: 26,
+            city: "Copenhagen"
+        }
+
+        await data.insertOne(doc).then((result) => {
+            console.log(result);
+            console.log("Document inserted");
+        });
+
+        await data.find().limit(2).toArray().then((result) => {
             console.log(result);
         });
 
+        await data.updateOne({name: "Sebastian"}, {$set: {name: "Sebastian", age: 27, city: "Copenhagen"}}).then((result) => {
+            console.log(result);
+            console.log("Document updated");
+        });
+
+        await data.find({name: {$eq: "Sebastian"}}).toArray().then((result) => {console.log(result)});
 
 
     } catch{
